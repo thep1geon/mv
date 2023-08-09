4b121c766de043bdbb6fcf1b473eb83643e6aeca
Magiic420 <hauptmaverick@gmail.com>
2023-08-09 18:01:06 -0400

    Optimized the test script

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import os
 
 def main():
+    os.system("./run")
     for _, _, files, in os.walk("examples/"):
         for name in files:
             if name.endswith(".mv"):
                 print(name)
                 print("Output: ")
-                os.system(f"./run examples/{name}")
+                os.system(f"./mv examples/{name}")
                 print("----------------------------------------------------")
                 print()
 
